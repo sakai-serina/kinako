dd98cca5c57c0eaf223b3a23a0025df514f1e553
kinakoserina <sase.0313@gmail.com>
2018-10-09 23:53:00 +0900

    change kinako

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    Usagi(knk="KAWAII is KINAKO")
+    Usagi(knk)
 </template>
 
 <script>
--- src/components/Usagi.vue
+++ src/components/Usagi.vue
@@ -6,7 +6,7 @@
 export default {
   name: "Usagi",
   props: {
-    knk: String
+    knk: "KAWAII is KINAKO"
   }
 };
 </script>
